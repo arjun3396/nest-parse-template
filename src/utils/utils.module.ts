0ab1cf6e53addacc00f48bf9689894b3bef934a8
Mandar <mkmandar123@gmail.com>
2021-02-18 17:14:52 +0530

    providers added in utils module

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthUtil } from './auth.util';
 import { CollectionUtil } from './collection.util';
 import { DB } from './db.util';
-import { DermtodoorError } from './dermtodoor-error.util';
 import { MongoDBConnection } from './mongo-db-connection.util';
 import { QueryUtil } from './query.util';
 import { Tree } from './tree.util';
+import { EventEmitterUtil } from './eventEmitter.util';
+import { SentryUtil } from './sentry.util';
+import { StorageUtil } from './storage.util';
 
 @Module({
-  providers: [AuthUtil, CollectionUtil, DB, MongoDBConnection, QueryUtil, Tree]
+  providers: [AuthUtil, CollectionUtil, DB, MongoDBConnection, QueryUtil, Tree, EventEmitterUtil, SentryUtil, StorageUtil]
 })
 export class UtilsModule {}
