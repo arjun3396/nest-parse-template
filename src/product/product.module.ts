f7943e5fc505e5f77d60252c8aa013d6ff6a8d60
Mandar <mkmandar123@gmail.com>
2021-02-18 17:25:20 +0530

    imports added to product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { ProductDto } from './dto/product.dto';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService, ProductDto]
+  providers: [ProductService, ProductDto],
+  imports: [
+    UtilsModule
+  ]
 })
 export class ProductModule {}
