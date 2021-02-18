74f3dfea4b54e1e7696f91d53511a94f81bd6cdd
Mandar <mkmandar123@gmail.com>
2021-02-18 17:21:41 +0530

    imports added to instant-checkup module

--- src/instant-checkup/instant-checkup.module.ts
+++ src/instant-checkup/instant-checkup.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { InstantCheckupService } from './instant-checkup.service';
 import { InstantCheckupController } from './instant-checkup.controller';
 import { InstantCheckupDto } from './dto/instant-checkup.dto';
+import { UtilsModule } from '../utils/utils.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
   providers: [InstantCheckupService, InstantCheckupDto],
-  controllers: [InstantCheckupController]
+  controllers: [InstantCheckupController],
+  imports: [
+    UtilsModule,
+    UserModule,
+  ]
 })
 export class InstantCheckupModule {}
