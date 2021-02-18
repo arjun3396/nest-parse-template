50a57e0d281cd11fc746ea21213c3a808da5fd63
Mandar <mkmandar123@gmail.com>
2021-02-18 17:09:04 +0530

    added product dto in module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { ProductDto } from './dto/product.dto';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService, ProductDto]
 })
 export class ProductModule {}
