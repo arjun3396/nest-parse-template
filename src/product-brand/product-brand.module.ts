efa296d7c35151adc278e5ce4bed49241496fb73
Mandar <mkmandar123@gmail.com>
2021-02-18 17:25:58 +0530

    imports added to product-brand module

--- src/product-brand/product-brand.module.ts
+++ src/product-brand/product-brand.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductBrandService } from './product-brand.service';
 import { ProductBrandController } from './product-brand.controller';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
   providers: [ProductBrandService],
-  controllers: [ProductBrandController]
+  controllers: [ProductBrandController],
+  imports: [
+    UtilsModule
+  ]
 })
 export class ProductBrandModule {}
