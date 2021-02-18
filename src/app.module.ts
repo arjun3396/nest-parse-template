d18cd30fa173aa0503e5500f3ad7a7c9f22407bd
Mandar <mkmandar123@gmail.com>
2021-02-18 10:27:09 +0530

    minor fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { ParseServerController } from './parse-server/parse-server.controller';
 import { QueryService } from './query/query.service';
 import { UserModule } from './user/user.module';
-import { TreeService } from './tree/tree.service';
-import { TreeController } from './tree/tree.controller';
 import { TreeModule } from './tree/tree.module';
 import { QuestionModule } from './question/question.module';
 import { ProductTypeModule } from './product-type/product-type.module';
 import { ProductModule } from './product/product.module';
 import { ProductBrandModule } from './product-brand/product-brand.module';
-import { ProductBranchService } from './product-branch/product-branch.service';
 import { OrderModule } from './order/order.module';
 import { NotificationModule } from './notification/notification.module';
 import { MainConcernModule } from './main-concern/main-concern.module';
@@ -20,7 +17,7 @@ import { CheckoutModule } from './checkout/checkout.module';
 
 @Module({
   imports: [UserModule, TreeModule, QuestionModule, ProductTypeModule, ProductModule, ProductBrandModule, OrderModule, NotificationModule, MainConcernModule, InstantCheckupModule, HelpModule, FavouriteModule, CheckoutModule],
-  controllers: [ParseServerController, TreeController],
-  providers: [QueryService, TreeService, ProductBranchService],
+  controllers: [ParseServerController],
+  providers: [QueryService],
 })
 export class AppModule {}
