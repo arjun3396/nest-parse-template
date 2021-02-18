ae27630676e591dd77dd525f96fd4df14bbae53b
Mandar <mkmandar123@gmail.com>
2021-02-18 17:20:49 +0530

    imports added to help module

--- src/help/help.module.ts
+++ src/help/help.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { HelpController } from './help.controller';
 import { HelpService } from './help.service';
 import { HelpDto } from './dto/help.dto';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
   controllers: [HelpController],
-  providers: [HelpService, HelpDto]
+  providers: [HelpService, HelpDto],
+  imports: [
+    UtilsModule
+  ]
 })
 export class HelpModule {}
