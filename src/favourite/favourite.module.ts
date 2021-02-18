80d61bb1c6f8a1ff60f5190c3271cd0ed55325b4
Mandar <mkmandar123@gmail.com>
2021-02-18 17:19:59 +0530

    imports added to favourite module

--- src/favourite/favourite.module.ts
+++ src/favourite/favourite.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { FavouriteService } from './favourite.service';
 import { FavouriteController } from './favourite.controller';
 import { FavouriteDto } from './dto/favourite.dto';
+import { UserModule } from '../user/user.module';
+import { UtilsModule } from '../utils/utils.module';
+import { CheckoutModule } from '../checkout/checkout.module';
 
 @Module({
   providers: [FavouriteService],
-  controllers: [FavouriteController, FavouriteDto]
+  controllers: [FavouriteController, FavouriteDto],
+  imports: [
+    UserModule,
+    UtilsModule,
+    CheckoutModule
+  ]
 })
 export class FavouriteModule {}
