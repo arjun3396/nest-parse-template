9e8a95f6a8173992bb1a97bd585be34d0a712ef9
Mandar <mkmandar123@gmail.com>
2021-02-18 17:10:18 +0530

    added favourite dto in module

--- src/favourite/favourite.module.ts
+++ src/favourite/favourite.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FavouriteService } from './favourite.service';
 import { FavouriteController } from './favourite.controller';
+import { FavouriteDto } from './dto/favourite.dto';
 
 @Module({
   providers: [FavouriteService],
-  controllers: [FavouriteController]
+  controllers: [FavouriteController, FavouriteDto]
 })
 export class FavouriteModule {}
