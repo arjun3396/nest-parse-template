6c91c2b476c59f96885e69e47a9f7d33cd849187
Mandar <mkmandar123@gmail.com>
2021-02-18 17:24:18 +0530

    imports added to order module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { OrderDto } from './dto/order.dto';
+import { ConsultationSessionModule } from '../consultation-session/consultation-session.module';
+import { UtilsModule } from '../utils/utils.module';
+import { UserModule } from '../user/user.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService, OrderDto]
+  providers: [OrderService, OrderDto],
+  imports: [
+    ConsultationSessionModule,
+    UtilsModule,
+    UserModule,
+    ProductModule
+  ]
 })
 export class OrderModule {}
