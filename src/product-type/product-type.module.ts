741d8bb18eaaa0aa569587caeb696400fcee3b78
Mandar <mkmandar123@gmail.com>
2021-02-18 17:26:27 +0530

    imports added to product-type module

--- src/product-type/product-type.module.ts
+++ src/product-type/product-type.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductTypeService } from './product-type.service';
 import { ProductTypeController } from './product-type.controller';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
   providers: [ProductTypeService],
-  controllers: [ProductTypeController]
+  controllers: [ProductTypeController],
+  imports: [
+    UtilsModule
+  ]
 })
 export class ProductTypeModule {}
