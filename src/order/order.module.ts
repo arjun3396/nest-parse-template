2566ab86705043533e28ca9fd38cbf722cb5a107
Mandar <mkmandar123@gmail.com>
2021-02-18 17:08:47 +0530

    added order dto in module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { OrderDto } from './dto/order.dto';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [OrderService, OrderDto]
 })
 export class OrderModule {}
