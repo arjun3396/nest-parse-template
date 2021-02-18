ca68c85150783ec0063ff1ea184e6f8b077fcd9b
Mandar <mkmandar123@gmail.com>
2021-02-18 17:05:57 +0530

    added question dto in module

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
+import { QuestionDto } from './dto/question.dto';
 
 @Module({
   controllers: [QuestionController],
-  providers: [QuestionService]
+  providers: [QuestionService, QuestionDto]
 })
 export class QuestionModule {}
