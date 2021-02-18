373e529c96b0f0181d37dfae6ad068e67eba96cc
Mandar <mkmandar123@gmail.com>
2021-02-18 17:18:45 +0530

    imports added to consultation-session module

--- src/consultation-session/consultation-session.module.ts
+++ src/consultation-session/consultation-session.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConsultationSessionService } from './consultation-session.service';
 import { ConsultationSessionDto } from './dto/consultation-session.dto';
+import { UtilsModule } from '../utils/utils.module';
+import { UserResponseModule } from '../user-response/user-response.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  providers: [ConsultationSessionService, ConsultationSessionDto]
+  providers: [ConsultationSessionService, ConsultationSessionDto],
+  imports: [
+    UtilsModule,
+    UserResponseModule,
+    UserModule
+  ]
 })
 export class ConsultationSessionModule {}
