80ff955b116839c46bb3b82d0031550d6135652b
Mandar <mkmandar123@gmail.com>
2021-02-18 17:30:57 +0530

    imports added to user-response module

--- src/user-response/user-response.module.ts
+++ src/user-response/user-response.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserResponseService } from './user-response.service';
 import { UserResponseDto } from './dto/user-response.dto';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
-  providers: [UserResponseService, UserResponseDto]
+  providers: [UserResponseService, UserResponseDto],
+  imports: [
+    UtilsModule
+  ]
 })
 export class UserResponseModule {}
