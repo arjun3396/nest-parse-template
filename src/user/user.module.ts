b3558c855738c3797786dee9ab369df325968813
Mandar <mkmandar123@gmail.com>
2021-02-18 17:30:16 +0530

    imports added to user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,20 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
 import { UtilsModule } from '../utils/utils.module';
+import { CheckoutModule } from '../checkout/checkout.module';
+import { OrderModule } from '../order/order.module';
+import { InstantCheckupModule } from '../instant-checkup/instant-checkup.module';
+import { FavouriteModule } from '../favourite/favourite.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService, UserDto],
-  imports: [UtilsModule]
+  imports: [
+    UtilsModule,
+    CheckoutModule,
+    OrderModule,
+    InstantCheckupModule,
+    FavouriteModule
+  ]
 })
 export class UserModule {}
