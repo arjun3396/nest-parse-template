66b9f68343a136db2b9898d9134e77399bf5a7c0
Mandar <mkmandar123@gmail.com>
2021-02-18 17:17:25 +0530

    imports added to checkout module

--- src/checkout/checkout.module.ts
+++ src/checkout/checkout.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { CheckoutController } from './checkout.controller';
 import { CheckoutService } from './checkout.service';
 import { CheckoutDto } from './dto/checkout.dto';
+import { UtilsModule } from '../utils/utils.module';
+import { UserModule } from '../user/user.module';
+import { OrderModule } from '../order/order.module';
+import { ConsultationSessionModule } from '../consultation-session/consultation-session.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
   controllers: [CheckoutController],
-  providers: [CheckoutService, CheckoutDto]
+  providers: [CheckoutService, CheckoutDto],
+  imports: [
+    UtilsModule,
+    UserModule,
+    OrderModule,
+    ConsultationSessionModule,
+    ProductModule
+  ]
 })
 export class CheckoutModule {}
