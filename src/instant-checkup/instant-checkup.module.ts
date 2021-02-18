da3abf7bc5e4279f5432306c2a35fa09c6a4284d
Mandar <mkmandar123@gmail.com>
2021-02-18 17:09:32 +0530

    added product dto in module

--- src/instant-checkup/instant-checkup.module.ts
+++ src/instant-checkup/instant-checkup.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InstantCheckupService } from './instant-checkup.service';
 import { InstantCheckupController } from './instant-checkup.controller';
+import { InstantCheckupDto } from './dto/instant-checkup.dto';
 
 @Module({
-  providers: [InstantCheckupService],
+  providers: [InstantCheckupService, InstantCheckupDto],
   controllers: [InstantCheckupController]
 })
 export class InstantCheckupModule {}
