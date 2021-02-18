46227624f6917d13c76e20b29951ccf85a89fc44
Mandar <mkmandar123@gmail.com>
2021-02-18 17:23:14 +0530

    imports added to notification module

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
   providers: [NotificationService],
-  controllers: [NotificationController]
+  controllers: [NotificationController],
+  imports: [UtilsModule]
 })
 export class NotificationModule {}
