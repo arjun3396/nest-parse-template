11d8a27d4f712edcaee82a244428b9a2396a086a
Mandar <mkmandar123@gmail.com>
2021-02-18 17:27:54 +0530

    imports added to question module

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
 import { QuestionDto } from './dto/question.dto';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
   controllers: [QuestionController],
-  providers: [QuestionService, QuestionDto]
+  providers: [QuestionService, QuestionDto],
+  imports: [UtilsModule]
 })
 export class QuestionModule {}
