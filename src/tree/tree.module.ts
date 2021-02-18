81ac402bc55ba3b8e6bc9a95d32d41c308aa2e1d
Mandar <mkmandar123@gmail.com>
2021-02-18 17:29:05 +0530

    imports added to tree module

--- src/tree/tree.module.ts
+++ src/tree/tree.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { TreeController } from './tree.controller';
 import { TreeService } from './tree.service';
 import { UtilsModule } from '../utils/utils.module';
+import { UserModule } from '../user/user.module';
+import { QuestionModule } from '../question/question.module';
+import { ConsultationSessionModule } from '../consultation-session/consultation-session.module';
 
 @Module({
   controllers: [TreeController],
   providers: [TreeService],
-  imports: [UtilsModule]
+  imports: [
+    UtilsModule,
+    UserModule,
+    QuestionModule,
+    ConsultationSessionModule
+  ]
 })
 export class TreeModule {}
