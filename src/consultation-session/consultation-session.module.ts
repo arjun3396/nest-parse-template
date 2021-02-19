109cea6f61a8a747ebb537fc8e9690a4c1022adc
Mandar <mkmandar123@gmail.com>
2021-02-19 11:20:16 +0530

    fixed all consultation-session circular dependencies

--- src/consultation-session/consultation-session.module.ts
+++ src/consultation-session/consultation-session.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConsultationSessionService } from './consultation-session.service';
 import { ConsultationSessionDto } from './dto/consultation-session.dto';
 import { UtilsModule } from '../utils/utils.module';
-import { UserResponseModule } from '../user-response/user-response.module';
-import { UserModule } from '../user/user.module';
 
 @Module({
   providers: [ConsultationSessionService, ConsultationSessionDto],
   imports: [
-    UtilsModule,
-    UserResponseModule,
-    UserModule
+    UtilsModule
   ]
 })
 export class ConsultationSessionModule {}
